fab0d2f4ca8e8736a3f1b48e5d55fecb626ced19
Michael Huynh <mike.huynh@gmail.com>
2010-08-06 17:15:17 -0400

    Moved worksheet vertical fill out into own function.

--- static/js/karuko.notebook.js
+++ static/js/karuko.notebook.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+function worksheet_vertical_fill() {
     //Take the window height and subtract the top position of the #worksheet
     //to find out how much more space we need to fill.
     var fill = $(window).height() - $('#worksheet').offset().top;
@@ -9,7 +9,13 @@ $(document).ready(function() {
     if (fill > 0) {
         $('#worksheet').css('height', fill);
     }
-    
+}
+
+$(document).ready(function() {
+    //If the worksheet length isn't long enough, extend it to fill the vertical
+    //height of the screen.
+    worksheet_vertical_fill();
+
     //NOTE: This only applies to existing textareas.
     $('#worksheet .entry textarea').autoResize({
         animate: false, 
