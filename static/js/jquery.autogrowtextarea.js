6d2b8c34b0f86a4bd337240c3e721e3037ab5fa1
Michael Huynh <mike.huynh@gmail.com>
2010-08-06 20:46:28 -0400

    jQuery-fied and added namespace to bind event.

--- static/js/jquery.autogrowtextarea.js
+++ static/js/jquery.autogrowtextarea.js
@@ -1,47 +1,44 @@
-//Private variables
-var colsDefault = 0;
-var rowsDefault = 0;
-//var rowsCounter = 0;
-
-//Private functions
-function setDefaultValues(txtArea)
-{
-	colsDefault = txtArea.cols;
-	rowsDefault = txtArea.rows;
-	//rowsCounter = document.getElementById("rowsCounter");
-}
-
-function bindEvents(txtArea)
-{
-	txtArea.onkeyup = function() {
-		grow(txtArea);
-	}
-}
-
-//Helper functions
-function grow(txtArea)
-{
-    var linesCount = 0;
-    var lines = txtArea.value.split('\n');
-
-    for (var i=lines.length-1; i>=0; --i)
-    {
-        linesCount += Math.floor((lines[i].length / colsDefault) + 1);
-    }
-
-    if (linesCount >= rowsDefault)
-        //HACK: Comment out +1 since we want the textarea to only be one row.
-        txtArea.rows = linesCount;// + 1;
-	else
-        txtArea.rows = rowsDefault;
-	//rowsCounter.innerHTML = linesCount + " | " + txtArea.rows;
-}
+(function($) {
 
 //Public Method
-jQuery.fn.autoGrow = function(){
-	return this.each(function(){
-		setDefaultValues(this);
-		bindEvents(this);
+$.fn.autoGrow = function() {
+    //Private variables
+    var colsDefault = 0;
+    var rowsDefault = 0;
+    //var rowsCounter = 0;
+    
+    //Helper functions
+    function grow(txtArea)
+    {
+        var linesCount = 0;
+        var lines = txtArea.value.split('\n');
+    
+        for (var i=lines.length-1; i>=0; --i)
+        {
+            linesCount += Math.floor((lines[i].length / colsDefault) + 1);
+        }
+    
+        if (linesCount >= rowsDefault)
+            //HACK: Comment out +1 since we want the textarea to only be one row.
+            txtArea.rows = linesCount;// + 1;
+    	else
+            txtArea.rows = rowsDefault;
+    	//rowsCounter.innerHTML = linesCount + " | " + txtArea.rows;
+    }
+
+    //Only work on textareas
+	this.filter('textarea').each(function() {
+        //Set default values
+    	colsDefault = this.cols;
+    	rowsDefault = this.rows;
+        
+        //Bind events
+    	$(this).bind('keyup.autogrow', function() {
+    		grow(this);
+    	});
 	});
+
+    return this;
 };
 
+})(jQuery);
